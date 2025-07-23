d7a82a5f96e3802bdbf5122aa52e651d9e9e7ec8
Quinn Blenkinsop <quinn@arcjet.com>
2025-07-23 17:02:36 +0000

    chore(fastify): Remove the plugin for simple decorate

--- examples/fastify/src/app.ts
+++ examples/fastify/src/app.ts
@@ -1,11 +1,16 @@
 import path from "node:path";
+import arcjetFastify, { type ArcjetFastify } from "@arcjet/fastify";
 import fastifyAutoLoad from "@fastify/autoload";
 import fastifyEnv from "@fastify/env";
 import type { FastifyInstance } from "fastify";
-import arcjetPlugin from "./plugins/arcjet.ts";
 
 declare module "fastify" {
   interface FastifyInstance {
+    // Tells TypeScript about the `arcjet` property we've decorated the Fastify
+    // instance with below.
+    arcjet: ArcjetFastify<unknown>;
+    // Tells TypeSscript about the `config` property provided by the
+    // `@fastify/env`plugin below.
     config: {
       ARCJET_KEY?: string;
     };
@@ -34,10 +39,20 @@ export default async function plugin(
     throw new Error("Missing the ARCJET_KEY environment variable.");
   }
 
-  await fastify.register(arcjetPlugin, {
-    key: fastify.config.ARCJET_KEY,
-    rules: [],
-  });
+  // Decorate the Fastify instance with the Arcjet plugin so
+  // it is always available at `fastify.arcjet`.
+  await fastify.decorate(
+    "arcjet",
+    arcjetFastify({
+      key: fastify.config.ARCJET_KEY,
+      // Here is where you could configure rules that would be applied to all
+      // of your routes that call `fastify.arcjet.protect()`. For this example,
+      // we set specific rules in the route handlers instead.
+      rules: [],
+      // Configures Arcjet to use the Fastify logger
+      log: fastify.log,
+    }),
+  );
 
   await fastify.register(fastifyAutoLoad, {
     dir: path.join(import.meta.dirname, "routes"),
